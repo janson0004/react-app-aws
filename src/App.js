2424b83b305295dd82c0b3328d1405fbb4e6601c
janson_cheung <jansoncheung0004@gmail.com>
2021-04-19 13:11:55 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world
-          <code>src/App.js</code> and save to reload.
-        </p>
+        <p>This is a new message!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
